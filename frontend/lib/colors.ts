baca92430d5e9faf14ba07f95e0d3b01e6574327
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-03 20:56:06 +0300

    feat: add color utility helpers

---
+++ frontend/lib/colors.ts
@@ -0,0 +1,7 @@
+export function hexToRgb(hex: string) {
+  const val = parseInt(hex.replace("#", ""), 16);
+  return { r: (val >> 16) & 255, g: (val >> 8) & 255, b: val & 255 };
+}
+export function uint24ToHex(color: number): string {
+  return "#" + color.toString(16).padStart(6, "0");
+}
